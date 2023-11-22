de703adb82d6d9f44605a3eb99c72a1df3609550
AnMorozov <anmorozov@it-one.ru>
2023-11-22 18:50:58 +0300

    remove dependency

--- L04-generics/build.gradle.kts
+++ L04-generics/build.gradle.kts
@@ -1,6 +1,4 @@
 dependencies {
-    implementation("org.jetbrains:annotations:23.0.0")
-    implementation("org.jetbrains:annotations:23.0.0")
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
     testImplementation ("org.assertj:assertj-core")
