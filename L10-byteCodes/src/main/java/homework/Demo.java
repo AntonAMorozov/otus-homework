c1618c638967c4907a832317a8e5bd83cc1708da
AnMorozov <anmorozov@it-one.ru>
2024-01-07 15:33:41 +0300

    hw-10 aop fixes

--- L10-byteCodes/src/main/java/homework/Demo.java
+++ L10-byteCodes/src/main/java/homework/Demo.java
@@ -4,13 +4,11 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        TestLoggingImpl testLoggingImpl = new TestLoggingImpl();
-        TestLogging testLogging = (TestLogging) Ioc.createTestLogging(testLoggingImpl);
+        var testLogging = (TestLogging) Ioc.createTestLogging(new TestLoggingImpl());
         testLogging.calculation(6); // логирование не вызывается
         testLogging.calculation(6, 7); // логирование вызывается
 
-        TestLoggingImpl2 testLoggingImpl2 = new TestLoggingImpl2();
-        TestLogging testLogging2 = (TestLogging) Ioc.createTestLogging(testLoggingImpl2);
+        var testLogging2 = (TestLogging) Ioc.createTestLogging( new TestLoggingImpl2());
         testLogging2.calculation(6); // логирование вызывается
         testLogging2.calculation(6, 7); // логирование не вызывается
     }
